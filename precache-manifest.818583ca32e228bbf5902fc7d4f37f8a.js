4d02fc8574e5509558ce42bdc300acc8136c4ad2
Paweł Pilarczyk <56913394+Pawel-Pilarczyk@users.noreply.github.com>
2020-09-04 21:47:07 +0200

    Updates

---
+++ precache-manifest.818583ca32e228bbf5902fc7d4f37f8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07069396b9848e50f8c02c98230453fe",
+    "url": "/budget-app/index.html"
+  },
+  {
+    "revision": "4eaae52bc5ba9d8d51b4",
+    "url": "/budget-app/static/css/main.ebde30b8.chunk.css"
+  },
+  {
+    "revision": "6bcd3f3aa103bb9b3888",
+    "url": "/budget-app/static/js/2.f78925aa.chunk.js"
+  },
+  {
+    "revision": "4db0b4b7fd2c7a0aaf4845dcd6ed7f11",
+    "url": "/budget-app/static/js/2.f78925aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4eaae52bc5ba9d8d51b4",
+    "url": "/budget-app/static/js/main.fb905a20.chunk.js"
+  },
+  {
+    "revision": "e2cd046d53fbad72425d",
+    "url": "/budget-app/static/js/runtime-main.1a3140f2.js"
+  }
+]);
